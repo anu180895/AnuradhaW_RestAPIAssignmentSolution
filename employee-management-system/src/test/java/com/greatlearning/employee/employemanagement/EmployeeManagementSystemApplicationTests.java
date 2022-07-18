10c36b6272e1e43c20ee0aec36a361f3836c8d76
anu180895 <101500035+anu180895@users.noreply.github.com>
2022-07-18 19:43:24 +0530

    Final Submission of RESTAPI assignment

---
+++ employee-management-system/src/test/java/com/greatlearning/employee/employemanagement/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatlearning.employee.employemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
